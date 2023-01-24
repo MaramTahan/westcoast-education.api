11506d28c556fc2affbd8c833c1b923389119f34
Maram Tahan <dr_maram.tahan@hotmail.com>
2023-01-24 09:13:04 +0100

    add coursecontroller

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -8,23 +8,63 @@ namespace westcoast_education.api.Controllers;
     {
         [HttpGet()]
         public ActionResult List(){
-            return Ok (new { message = "List work"});
+            //ok creates a json result with the status code 200
+            return Ok(new{
+                message= "Courses list done"
+            });
         }
 
         [HttpGet("{id}")]
+        //http://localhost:5188/api/c1/courses/5
 
         public ActionResult GetById(int id){
-            return Ok(new {message ="GetById work"});
+            return Ok(new{
+                message= $"GetById done {id}"
+            });
         }
 
-        [HttpGet("getbycourseno/{courseNo}")]
-
-        public ActionResult GetByCourseNo(string courseNo){
-            return Ok(new {message ="GetByCourseNo work"});
+        [HttpGet("courseno/{courseNo}")]
+        public ActionResult GetBycourseNumber(string courseNo){
+            return Ok (new{ message= $"GetBycourseNumber done {courseNo}"});
         }
+
+        [HttpGet("name/{name}")]
+        public ActionResult GetByname(string name){
+            return Ok (new{ message= $"GetByname done {name}"});
+        }
+
+        [HttpGet("startdate/{startDate}")]
+        public ActionResult GetByStartDate(string startDate){
+            return Ok (new{ message= $"GetByStartDate done {startDate}"});
+        }
+
         [HttpPost()]
-        public ActionResult Add(object data){
-            return Created(nameof(List), data);
+        public ActionResult AddCourse(){
+            return Created(nameof(GetById), new{
+                message = "AddCourse work"
+            });
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult UpdateCourse(int id){
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult DeleteCourse(int id){
+            //go to database and delete course
+            return NoContent();
+        }
+
+        [HttpPatch("fullybooked/{id}")]
+        //go to database for mark course as fully booked
+        public ActionResult MarkAsFullyBooked(int id){
+            return NoContent();
+        }
+
+        [HttpPatch("done/{id}")]
+        public ActionResult MarkAsDone(int id){
+            return NoContent();
         }
 
 
